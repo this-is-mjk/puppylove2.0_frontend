1f1ccf11f1f667ff1feb6dc8bf0b1c4bf61973ba
AshNewar <jnvspr12345@gmail.com>
2023-09-29 02:57:05 +0530

    Animation

--- pages/register.tsx
+++ pages/register.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styles from '../styles/login.module.css'; // Import your CSS module
 import {MdEmail} from 'react-icons/md'
 import {BiSolidLock} from 'react-icons/bi'
+import {BsFillPersonFill,BsPersonVcardFill} from 'react-icons/bs'
 import Image from 'next/image';
 import "../app/globals.css"
 import Link from 'next/link';
@@ -11,39 +12,56 @@ import Dog from '@/components/Dog';
 
 
 
-const LoginPage: React.FC = () => {
-  const [data,setData] =useState({email:"",password:""});
-  
+const RegisterPage: React.FC = () => {
+  const [data,setData] =useState({username:"",rollNo:"",email:"",password:""});
 
+  
   const handleLog=()=>{
     console.log(data);
-    setData({email:"",password:''});
+    setData({username:"",rollNo:"",email:"",password:""});
     console.log(data);
 
   }
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Perform authentication here, e.g., sending a request to a server
-    // with the email and password for verification.
-
-    // Reset form fields after submission
-    // setEmail('');
-    // setPassword('');
+    
     const {name,value} =e.target;
     setData({...data,[name]:value});
     console.log(data);
-    // handleLog();
   };
 
   return (
     <div className={styles['login-box']}>
-      
       <div className={styles['login-box2']}>
         <Dog/>
          <div className={styles['login-container']}>
-          <h1 className={styles['login-title']}>Login</h1>
+          <h1 className={styles['login-title']}>Register</h1>
+          <motion.div whileHover={{scale:1.05}} className={styles['login-form-group']}>
+            <BsFillPersonFill size={18}/>
+
+              <input
+                className={styles['login-input']}
+                type="text"
+                name='username'
+                value={data.username}
+                onChange={handleSubmit}
+                required
+                placeholder='UserName'
+              />
+            </motion.div>
+          <motion.div whileHover={{scale:1.05}} className={styles['login-form-group']}>
+            <BsPersonVcardFill size={18}/>
+
+              <input
+                className={styles['login-input']}
+                type="number"
+                name='rollNo'
+                value={data.rollNo}
+                onChange={handleSubmit}
+                required
+                placeholder='Roll No.'
+              />
+            </motion.div>
             <motion.div whileHover={{scale:1.05}} className={styles['login-form-group']}>
             <MdEmail size={18}/>
 
@@ -71,13 +89,13 @@ const LoginPage: React.FC = () => {
             </motion.div>
             <div className={styles['login-bottom']}>
               <motion.div whileHover={{scale:1.1}} whileTap={{scale:0.9}} className={styles['login-submit-button']} >
-              <Link href={"/register"}>
-              Register
+              <Link href={"/login"}>
+              Login
             </Link>
               </motion.div>
             
             <motion.div whileHover={{scale:1.1}} whileTap={{scale:0.9}} className={styles['login-submit-button']} onClick={handleLog} >
-              Login
+              Register
             </motion.div>
             </div>
             
@@ -88,4 +106,4 @@ const LoginPage: React.FC = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
