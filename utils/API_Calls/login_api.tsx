7c4486995ffd21616ad3d060b4fb183dea6cff38
its-me-yps <yps8181@gmail.com>
2023-10-16 16:57:38 +0530

    Setting User Data on Login

--- utils/API_Calls/login_api.tsx
+++ utils/API_Calls/login_api.tsx
@@ -1,4 +1,5 @@
-import {SHA256} from "../Encryption"
+import {SHA256, Decryption_AES} from "../Encryption"
+import {Set_PrivK, Set_PubK, Set_Data} from "../UserData"
 const SERVER_IP = process.env.SERVER_IP
 
 export const handleLog = async(data: any) => {
@@ -7,8 +8,9 @@ export const handleLog = async(data: any) => {
       const res = await fetch(
           SERVER_IP+"/session/login", {
               method: "POST",
+              credentials: "include",
               body: JSON.stringify({
-                  "_id": data.email,
+                  "_id": data.id,
                   "passHash": passHash
               }),
 
@@ -20,9 +22,13 @@ export const handleLog = async(data: any) => {
       if (!res.ok) {
         throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
       }
-      else {
-        return true
-      }
+      const res_json = await res.json()
+      const pvtKey_Enc: string = res_json.pvtKey_Enc
+      const pvtKey_login = await Decryption_AES(pvtKey_Enc, data.password)
+      Set_PrivK(pvtKey_login)
+      Set_PubK(res_json.pubKey)
+      await Set_Data(res_json.data)
+      return true
     }
     catch(err) {
       console.log(err)
