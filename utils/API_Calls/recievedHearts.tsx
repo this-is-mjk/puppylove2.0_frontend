6ff22b0d59f91ab79b2f1c64bc8893335d49a3e8
Shreya <shreyaashree.17@gmail.com>
2023-11-01 05:08:22 +0530

    Added feature to fetch data only on login

--- utils/API_Calls/recievedHearts.tsx
+++ utils/API_Calls/recievedHearts.tsx
@@ -20,61 +20,127 @@ export function Set_heartsFemale(heartsFemales : number) {
 }
 
 
-export const receivedHeart = async() => {
-    try{
-        const res = await fetch(`${SERVER_IP}/fetchall`, {
-            method: "GET"
-        });
-        if (!res.ok) {
-            throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
-        }else{
-            const data = await res.json()
-            decodedHearts = data.map((element : any) => {
-                const encoded_sha = element.enc
-                const gender = element.gender_of_sender
-                const decrypted_sha = Decryption(encoded_sha, PrivK)
+// Import necessary modules and variables
 
-                return {
-                    enc : decrypted_sha,
-                    genderOfSender : gender
-                }
-            })
-        }
+export const fetchAllOnLogin = async () => {
+  try {
+    // Fetch data and decode hearts
+    const decodedHearts = await fetchAndDecodeHearts();
+    console.log("fetched")
+    
+    // Send decoded hearts
+    await sendDecodedHearts(decodedHearts);
+  } catch (error) {
+    console.error('An error occurred during login:', error);
+  }
+};
 
-    }catch (err){
-        console.error('Error while fetching and decrypting data', err);
+const fetchAndDecodeHearts = async () => {
+  try {
+    const res = await fetch(`${SERVER_IP}/fetchall`, {
+      method: "GET",
+    });
+    if (!res.ok) {
+      throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
+    } else {
+      const data = await res.json();
+      const decodedHearts = data.map((element : any) => {
+        const encoded_sha = element.enc;
+        const gender = element.gender_of_sender;
+        const decrypted_sha = Decryption(encoded_sha, PrivK);
+
+        return {
+          enc: decrypted_sha,
+          genderOfSender: gender,
+        };
+      });
+      return decodedHearts;
     }
-}
+  } catch (err) {
+    throw new Error('Error while fetching and decrypting data: ' + err);
+  }
+};
+
+const sendDecodedHearts = async (decodedHearts : any) => {
+  try {
+    const response = await fetch(`${SERVER_IP}/sentHeartDecoded`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ DecodedHearts: decodedHearts }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      Set_heartsMale(data.male);
+      Set_heartsFemale(data.female);
+    } else {
+      throw new Error('Failed to send decoded hearts');
+    }
+  } catch (error) {
+    throw new Error('Error while sending decoded hearts: ' + error);
+  }
+};
+
+// // Call the login function during login
+// login();
+
+// export const receivedHeart = async() => {
+//     try{
+//         const res = await fetch(`${SERVER_IP}/fetchall`, {
+//             method: "GET"
+//         });
+//         if (!res.ok) {
+//             throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
+//         }else{
+//             const data = await res.json()
+//             decodedHearts = data.map((element : any) => {
+//                 const encoded_sha = element.enc
+//                 const gender = element.gender_of_sender
+//                 const decrypted_sha = Decryption(encoded_sha, PrivK)
+
+//                 return {
+//                     enc : decrypted_sha,
+//                     genderOfSender : gender
+//                 }
+//             })
+//         }
+
+//     }catch (err){
+//         console.error('Error while fetching and decrypting data', err);
+//     }
+// }
 
 
-export const sendDecordedSHA = async (decodedHearts: any) => {
-    try {
-        const response = await fetch(`${SERVER_IP}/sentHeartDecoded`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ DecodedHearts: decodedHearts }),
-        });
+// export const sendDecordedSHA = async (decodedHearts: any) => {
+//     try {
+//         const response = await fetch(`${SERVER_IP}/sentHeartDecoded`, {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({ DecodedHearts: decodedHearts }),
+//         });
   
-        if (response.ok) {
-          const data = await response.json();
-          Set_heartsMale(data.male)
-          Set_heartsFemale(data.female)
-        } else {
-          console.error('Failed to send decoded hearts');
-        }
-    } catch (error) {
-        console.error('Error while sending decoded hearts', error);
-    }
-}
+//         if (response.ok) {
+//           const data = await response.json();
+//           Set_heartsMale(data.male)
+//           Set_heartsFemale(data.female)
+//         } else {
+//           console.error('Failed to send decoded hearts');
+//         }
+//     } catch (error) {
+//         console.error('Error while sending decoded hearts', error);
+//     }
+// }
 
 
-receivedHeart()
-  .then(() => {
-    sendDecordedSHA(decodedHearts);
-  })
-  .catch((err) => {
-    console.error('An error occurred:', err);
-  });
+// receivedHeart()
+//   .then(() => {
+//     sendDecordedSHA(decodedHearts);
+//   })
+//   .catch((err) => {
+//     console.error('An error occurred:', err);
+//   });
 
