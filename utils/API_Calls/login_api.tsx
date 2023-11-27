81a1c2406c681d8d385c1af7e08fa0b93340066b
its-me-yps <yps8181@gmail.com>
2023-11-27 15:53:37 +0530

    Set_Gender on Login

--- utils/API_Calls/login_api.tsx
+++ utils/API_Calls/login_api.tsx
@@ -1,36 +1,41 @@
-import { SHA256, Decryption_AES } from "../Encryption"
-import { Set_PrivK, Set_PubK, Set_Data } from "../UserData"
+import {SHA256, Decryption_AES} from "../Encryption"
+import {Set_PrivK, Set_PubK, Set_Data, Set_Gender} from "../UserData"
+// import { fetchAllOnLogin } from "./recievedHearts"
 const SERVER_IP = process.env.SERVER_IP
 
-export const handleLog = async (data: any) => {
-  try {
-    const passHash = await SHA256(data.password)
-    const res = await fetch(
-      SERVER_IP + "/session/login", {
-      method: "POST",
-      body: JSON.stringify({
-        "_id": data.id,
-        "passHash": passHash
-      }),
+export const handleLog = async(data: any) => {
+    try {
+      const passHash = await SHA256(data.password)
+      const res = await fetch(
+          `${SERVER_IP}/session/login`, {
+              method: "POST",
+              credentials: "include",
+              body: JSON.stringify({
+                  _id: data.id,
+                  passHash: passHash
+              }),
 
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
+              headers: {
+                  "Content-type": "application/json; charset=UTF-8"
+              }
+          }
+      );
+      if (!res.ok) {
+        throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
       }
+      const res_json = await res.json()
+      const pvtKey_Enc: string = res_json.pvtKey_Enc
+      const pvtKey_login = await Decryption_AES(pvtKey_Enc, data.password)
+
+      Set_PrivK(pvtKey_login)
+      Set_PubK(res_json.pubKey)
+      Set_Gender(res_json.gender)
+      await Set_Data(res_json.data)
+      // await fetchAllOnLogin()
+      return true
     }
-    );
-    if (!res.ok) {
-      throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
+    catch(err) {
+      console.log(err)
+      return false
     }
-    const res_json = await res.json()
-    const pvtKey_Enc: string = res_json.pvtKey_Enc
-    const pvtKey_login = await Decryption_AES(pvtKey_Enc, data.password)
-    Set_PrivK(pvtKey_login)
-    Set_PubK(res_json.pubKey)
-    await Set_Data(res_json.data)
-    return true
   }
-  catch (err) {
-    console.log(err)
-    return false
-  }
-}
