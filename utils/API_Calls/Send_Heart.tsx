0ed9cbb110107b6b2c4896f61d939648745ce8bc
its-me-yps <yps8181@gmail.com>
2023-11-27 15:54:08 +0530

    Submit Hearts

--- utils/API_Calls/Send_Heart.tsx
+++ utils/API_Calls/Send_Heart.tsx
@@ -1,95 +1,121 @@
-import { SHA256, Encryption, RandInt, Decryption } from "../Encryption"
-import { PubK } from "../UserData"
+import {SHA256, Encryption, RandInt} from "../Encryption"
+import { PubK, Gender } from "../UserData"
 const SERVER_IP = process.env.SERVER_IP
 
-export const SendHeart = async (senderId: string, receiverIds: string[]) => {
+export const SendHeart = async(senderId: string, receiverIds: string [], Submit: boolean) => {
     try {
-        const enc: string[] = []
-        const sha: string[] = []
+        const enc : string[] = []
+        const sha : string[] = []
         const ids_encrypt: string[] = []
         const R1: number = parseInt(senderId)
-        for (const id of receiverIds) {
+        for(const id of receiverIds) {
             const R2: number = parseInt(id)
             const random = await RandInt()
             const R3: string = random.toString()
-            const pubKey: string | null = await get_pubKey(id)
-            if (pubKey === null) {
+            const pubKey: string|null = await get_pubKey(id)
+            if(pubKey === null) {
                 throw new Error("Error")
             }
-            if (R1 < R2) {
-                const sha_: string = await SHA256(R1.toString() + R2.toString() + R3)
+            if(R1 < R2) {
+                const sha_:string = await SHA256(R1.toString() + R2.toString() + R3)
                 sha.push(sha_)
-                const enc_: string = await Encryption(sha_, pubKey)
+                const enc_:string = await Encryption(sha_, pubKey)
                 enc.push(enc_)
             }
             else {
-                const sha_: string = await SHA256(R2.toString() + R1.toString() + R3)
+                const sha_:string = await SHA256(R2.toString() + R1.toString() + R3)
                 sha.push(sha_)
-                const enc_: string = await Encryption(sha_, pubKey)
+                const enc_:string = await Encryption(sha_, pubKey)
                 enc.push(enc_)
             }
-            const id_encrypt: string = await Encryption(R2.toString() + '+' + R3, PubK)
+            const id_encrypt:string = await Encryption(R2.toString() + '+' + R3, PubK)
             ids_encrypt.push(id_encrypt)
         }
-        const res = await fetch(
-            `${SERVER_IP}/users/sendheartVirtual`, {
-            method: "POST",
-            body: JSON.stringify({
-                "hearts": {
-                    "heart1": {
-                        "enc": enc[0],
-                        "sha": sha[0],
-                        "id_encrypt": ids_encrypt[0]
-                    },
-                    "heart2": {
-                        "enc": enc[1],
-                        "sha": sha[1],
-                        "id_encrypt": ids_encrypt[1]
-                    },
-                    "heart3": {
-                        "enc": enc[2],
-                        "sha": sha[2],
-                        "id_encrypt": ids_encrypt[2]
-                    },
-                    "heart4": {
-                        "enc": enc[3],
-                        "sha": sha[3],
-                        "id_encrypt": ids_encrypt[3]
-                    }
+        if(!Submit) {
+            const res = await fetch(
+                `${SERVER_IP}/users/sendheartVirtual`, {
+                    method: "POST",
+                    credentials: "include",
+                    body: JSON.stringify({
+                        hearts: {
+                            heart1: {
+                                enc: enc[0],
+                                sha: sha[0],
+                                id_encrypt: ids_encrypt[0]
+                            },
+                            heart2: {
+                                enc: enc[1],
+                                sha: sha[1],
+                                id_encrypt: ids_encrypt[1]
+                            },
+                            heart3: {
+                                enc: enc[2],
+                                sha: sha[2],
+                                id_encrypt: ids_encrypt[2]
+                            },
+                            heart4: {
+                                enc: enc[3],
+                                sha: sha[3],
+                                id_encrypt: ids_encrypt[3]
+                            }
+                        }
+                    }),
                 }
-            }),
+            );
+            if (!res.ok) {
+                throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
+            }
         }
-        );
-        if (!res.ok) {
-            throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
+        else {
+            const res = await fetch(
+                `${SERVER_IP}/users/sendheart`, {
+                    method: "POST",
+                    credentials: "include",
+                    body: JSON.stringify({
+                        genderofsender: Gender,
+                        enc1: enc[0],
+                        sha1: sha[0],
+                        enc2: enc[1],
+                        sha2: sha[1],
+                        enc3: enc[2],
+                        sha3: sha[2],
+                        enc4: enc[3],
+                        sha4: sha[4]
+                    }),
+                }
+            );
+            if (!res.ok) {
+                throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
+            }
         }
         return true
     }
-    catch (err) {
+    catch(err) {
         console.log(err)
         return false
     }
-}
+  }
 
-async function get_pubKey(id: string) {
+  async function get_pubKey (id: string) {
     try {
         const res = await fetch(
             `${SERVER_IP}/users/fetchPublicKeys`, {
-            method: "GET",
-        }
-        )
-        if (!res.ok) {
-            throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
-        }
-        const res_json = await res.json()
-        for (const publicKey of res_json) {
-            if (publicKey._id === id) {
-                return publicKey.pubKey;
+                method: "GET",
+                credentials: "include"
             }
-        }
-        throw new Error("Public Key Not Found")
+            )
+            if (!res.ok) {
+                throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
+            }
+            const res_json = await res.json()
+            for (const publicKey of res_json) {
+                if (publicKey._id === id) {
+                    return publicKey.pubKey;
+                }
+            }
+            throw new Error("Public Key Not Found")
     }
-    catch (err) {
+    catch(err) {
         console.log(err)
     }
-}
+  }
