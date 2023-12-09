4c8c5b90c6151c35fbb69a7061998953178ef497
its-me-yps <yps8181@gmail.com>
2023-12-09 14:22:19 +0530

    Inactive User's Card

--- components/card.tsx
+++ components/card.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../styles/card.css";
 
-const Card = ({ student, onClick, clickedCheck }: any) => {
+const Card = ({ student, onClick, clickedCheck, isActive }: any) => {
   const userName = student.u;
   const roll = student.i;
 
@@ -20,17 +20,25 @@ const Card = ({ student, onClick, clickedCheck }: any) => {
   };
 
   return (
-    <div className="card">
+    <div className={`card ${isActive(student.i) ? '' : 'inactive'}`}>
       <div className="image-box">
         <div className="profile" style={stylesss}></div>
       </div>
       <p className="card-details">{student.n}</p>
 
+      {isActive(student.i) ? (
+        <div className="carddetails">
+          <button className="button" onClick={clicked} disabled={clickedCheck}>
+            {clickedCheck ? "Selected" : "Send Heart"}
+          </button>
+        </div>
+      ) : (
       <div className="carddetails">
-        <button className="button" onClick={clicked} disabled={clickedCheck}>
-          {clickedCheck ? "Selected" : "Send Heart"}
+        <button className="button" disabled>
+          Not Registered
         </button>
       </div>
+      )}
     </div>
   );
 };
