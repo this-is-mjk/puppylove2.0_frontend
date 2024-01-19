b9d1c580cd8142f6db7d0e6e7cdc435f2ef67ee8
its-me-yps <yps8181@gmail.com>
2024-01-19 17:56:18 +0530

    Block confirmation after results published

--- utils/API_Calls/confirmation_api.tsx
+++ utils/API_Calls/confirmation_api.tsx
@@ -2,20 +2,21 @@ const SERVER_IP = process.env.SERVER_IP
 
 export const confirmationToPublish = async() => {
     try {
-        const response = await fetch(`${SERVER_IP}/users/publish`, {
+        const res = await fetch(`${SERVER_IP}/users/publish`, {
           method: 'POST',
           credentials:'include',
           headers: {
             'Content-Type': 'application/json',
           },
         });
-        if (response.ok) {
-          console.log('API request successful');
-        
-        } else {
-          console.error('API request failed');
+        if (res.ok) {
+          return {"success": true, "error":""}
         }
+        console.log(`HTTP Error: ${res.status} - ${res.statusText}`);
+        const res_json = await res.json()
+        return {"success": false, "error": res_json.error as string}
     } catch (error) {
         console.error('Error during API request', error);
+        return {"success": false, "error": error}
     } 
 }
